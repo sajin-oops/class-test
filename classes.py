59600aacf29055fbc22b976ca60974673022cfda
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-28 22:47:11 +0530

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,10 @@
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
+
+# - <__main__.Person object at 0x000002150F4B40D0>
